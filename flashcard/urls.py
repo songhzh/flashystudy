eaec4c1bf2039f6b31fca987cbaa57a301ea392e
Song Hui Zhang <songhui@ualberta.ca>
2018-05-06 20:34:53 -0600

    add html

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'flashcard'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.StudyView.as_view(), name='study'),
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('select/', views.SelectView.as_view(), name='select'),
+    path('create/<int:pk>/', views.CreateView.as_view(), name='create'),
+    path('study/<int:pk>/', views.StudyView.as_view(), name='study'),
+    path('newdeck/', views.newDeck, name='newdeck'),
 ]
--- flashcard/views.py
+++ flashcard/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
+from django.contrib import messages
 from .models import Deck, Card
 
-class IndexView(generic.ListView):
-    model = Deck
+
+class IndexView(generic.TemplateView):
     template_name = 'flashcard/index.html'
+
+class SelectView(generic.ListView):
+    model = Deck
+    template_name = 'flashcard/select.html'
     context_object_name = 'deck_list'
 
 class StudyView(generic.DetailView):
     model = Deck
     template_name = 'flashcard/study.html'
+
+class CreateView(generic.TemplateView):
+    template_name = 'flashcard/create.html'
+
+def newDeck(request):
+    if Deck.objects.filter(deck_name__iexact='math').exists():
+        messages.error(request, 'Deck with the same name already exists')
+        return redirect('flashcard:select')
+    else:
+        return
